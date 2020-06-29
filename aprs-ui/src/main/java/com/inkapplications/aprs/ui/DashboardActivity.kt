4818a9f2de42793d9603e82b8473acc06a474d07
Renee Vandervelde <Renee@ReneeVandervelde.com>
2020-06-29 16:56:46 -0500

    Add dashboard activity

---
+++ aprs-ui/src/main/java/com/inkapplications/aprs/ui/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package com.inkapplications.aprs.ui
+
+import android.app.Activity
+import android.os.Bundle
+import com.inkapplications.aprs.R
+
+class DashboardActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.dashboard)
+    }
+}
