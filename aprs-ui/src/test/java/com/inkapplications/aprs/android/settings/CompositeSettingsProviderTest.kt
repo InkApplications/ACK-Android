5e6b049ebdacee3a087f07b375ebc9d5132b7c75
Renee Vandervelde <Renee@ReneeVandervelde.com>
2020-08-23 10:56:53 -0500

    Test composite settings provider

---
+++ aprs-ui/src/test/java/com/inkapplications/aprs/android/settings/CompositeSettingsProviderTest.kt
@@ -0,0 +1,34 @@
+package com.inkapplications.aprs.android.settings
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CompositeSettingsProviderTest {
+    @Test
+    fun getSettings() {
+        val stringSetting = StringSetting("test.string", "test", "category", "default")
+        val intSetting = IntSetting("test.int", "test", "category", 1234)
+        val first = object: SettingsProvider {
+            override val settings: List<Setting> = listOf(stringSetting)
+        }
+        val second = object: SettingsProvider {
+            override val settings: List<Setting> = listOf(intSetting)
+        }
+        val provider = CompositeSettingsProvider(setOf(first, second))
+
+        val result = provider.settings
+
+        assertTrue("Both settings are in combined result", stringSetting in result)
+        assertTrue("Both settings are in combined result", intSetting in result)
+    }
+
+    @Test
+    fun emptySettings() {
+        val provider = CompositeSettingsProvider(setOf())
+
+        val result = provider.settings
+
+        assertTrue("No settings providers results in empty set", result.isEmpty())
+    }
+}
