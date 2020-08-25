a812c835fe338ddf7de1dba3b39ea2298739197e
Renee Vandervelde <Renee@ReneeVandervelde.com>
2020-08-25 07:21:50 -0500

    Fix String Item bindings

--- aprs-ui/src/main/java/com/inkapplications/aprs/android/settings/SettingStringItem.kt
+++ aprs-ui/src/main/java/com/inkapplications/aprs/android/settings/SettingStringItem.kt
@@ -7,10 +7,10 @@ import kotlinx.android.synthetic.main.setting_item_string.*
 
 class SettingStringItem(val viewModel: SettingViewModel, val setting: StringSetting): Item() {
     override fun getId(): Long = setting.hashCode().toLong()
-    override fun getLayout(): Int = R.layout.setting_item_int
+    override fun getLayout(): Int = R.layout.setting_item_string
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         viewHolder.setting_item_string_name.text = viewModel.name
-        viewHolder.setting_item_string_name.text = viewModel.value
+        viewHolder.setting_item_string_value.text = viewModel.value
     }
 }
