ab55be14ad495f21584304d762410ac65cb61715
Renee Vandervelde <Renee@ReneeVandervelde.com>
2022-01-23 12:51:54 -0600

    License as GPLv2

--- android-application/src/main/java/com/inkapplications/ack/android/onboard/UsageAgreement.kt
+++ android-application/src/main/java/com/inkapplications/ack/android/onboard/UsageAgreement.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.inkapplications.ack.android.ui.theme.AprsTheme
 
-const val AGREEMENT_REVISION = 1
+const val AGREEMENT_REVISION = 2
 
 @Composable
 fun UsageAgreement(
@@ -30,10 +30,10 @@ fun UsageAgreement(
         )
         Spacer(Modifier.height(AprsTheme.spacing.item))
         Text(
-            "This software provides access and tools for the amateur radio APRS network, a legally regulated service."
+            "This software provides access and tools for the amateur radio APRS network. Radio communications are a regulated service."
         )
         Text(
-            "Some of the functionality provided by this application may require a license or be restricted in your area. It is your responsibility to research and follow local laws when using this software."
+            "Some of the functionality provided by this application can be used in ways that may require a license or be restricted in your area. It is your responsibility to research and follow local laws when using this software."
         )
         Spacer(Modifier.height(AprsTheme.spacing.content))
         Text(
@@ -48,6 +48,7 @@ fun UsageAgreement(
         Text(
             "This application collects usage data using Google Firebase. Information collected may include information unique to your device, information about actions performed within the application, and logging data related to using the application. This information is used internally to improve the application and is not shared with any 3rd parties."
         )
+
         Spacer(Modifier.height(AprsTheme.spacing.content))
         Text(
             "No Warranty",
@@ -57,6 +58,40 @@ fun UsageAgreement(
         Text(
             "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
         )
+
+        Spacer(Modifier.height(AprsTheme.spacing.content))
+        Text(
+            "Software Rights",
+            style = AprsTheme.typography.h2,
+        )
+        Spacer(Modifier.height(AprsTheme.spacing.item))
+        Text("Copyright (c) 2020-2022 Ink Applications")
+        Text(
+            """
+                This program is free software; you can redistribute it and/or modify
+                it under the terms of the GNU General Public License as published by
+                the Free Software Foundation; either version 2 of the License, or
+                (at your option) any later version.
+            """.trimIndent().replace('\n', ' ')
+        )
+        Text(
+            """
+                This program is distributed in the hope that it will be useful,
+                but WITHOUT ANY WARRANTY; without even the implied warranty of
+                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                GNU General Public License for more details.
+            """.trimIndent().replace('\n', ' ')
+        )
+        Text(
+            """
+                The full license can be obtained at:
+                http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+                The full license, as well as sources can be obtained at:
+                https://ack.inkapplications.com
+                or by emailing: legal@inkapplications.com
+            """.trimIndent().replace('\n', ' ')
+        )
+
         Spacer(Modifier.weight(1f))
         Button(
             onClick = controller::onTermsDeclineClick,
