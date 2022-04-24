2acf7c93f985a5fc36bf1df533a59c48828d6a71
Renee Vandervelde <Renee@ReneeVandervelde.com>
2022-04-24 08:19:13 -0500

    Format message dates

--- android-extensions/src/main/java/com/inkapplications/android/extensions/format/AndroidResourceDateTimeFormatter.kt
+++ android-extensions/src/main/java/com/inkapplications/android/extensions/format/AndroidResourceDateTimeFormatter.kt
@@ -3,8 +3,10 @@ package com.inkapplications.android.extensions.format
 import com.inkapplications.android.extensions.R
 import com.inkapplications.android.extensions.StringResources
 import dagger.Reusable
+import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import java.text.SimpleDateFormat
 import javax.inject.Inject
 
@@ -14,11 +16,27 @@ import javax.inject.Inject
 @Reusable
 class AndroidResourceDateTimeFormatter @Inject constructor(
     stringResources: StringResources,
-    timeZone: TimeZone,
+    private val timeZone: TimeZone,
+    private val clock: Clock,
 ): DateTimeFormatter {
-    private val timestampFormat = SimpleDateFormat(stringResources.getString(R.string.datetime_format_timestamp)).also {
+    private val timeFormat = SimpleDateFormat(stringResources.getString(R.string.datetime_format_time)).also {
+        it.timeZone = java.util.TimeZone.getTimeZone(timeZone.id)
+    }
+    private val dateTimeFormat = SimpleDateFormat(stringResources.getString(R.string.datetime_format_datetime)).also {
+        it.timeZone = java.util.TimeZone.getTimeZone(timeZone.id)
+    }
+    private val fullTimestampFormat = SimpleDateFormat(stringResources.getString(R.string.datetime_format_timestamp)).also {
         it.timeZone = java.util.TimeZone.getTimeZone(timeZone.id)
     }
 
-    override fun formatTimestamp(instant: Instant): String = instant.toEpochMilliseconds().let(timestampFormat::format)
+    override fun formatTimestamp(instant: Instant): String  {
+        val now = clock.now().toLocalDateTime(timeZone)
+        val local = instant.toLocalDateTime(timeZone)
+
+        return when {
+            local.year == now.year && local.dayOfYear == now.dayOfYear -> timeFormat.format(instant.toEpochMilliseconds())
+            local.year == now.year && local.month == now.month -> dateTimeFormat.format(instant.toEpochMilliseconds())
+            else -> fullTimestampFormat.format(instant.toEpochMilliseconds())
+        }
+    }
 }
