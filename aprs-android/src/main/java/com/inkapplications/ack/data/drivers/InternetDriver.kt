f27a57d86a672e09dd531a9933455fb3183a6e7a
Renee Vandervelde <Renee@ReneeVandervelde.com>
2022-05-30 08:42:00 -0500

    Add retry with exponenetial backoff to APRS-IS connection

--- aprs-android/src/main/java/com/inkapplications/ack/data/drivers/InternetDriver.kt
+++ aprs-android/src/main/java/com/inkapplications/ack/data/drivers/InternetDriver.kt
@@ -8,15 +8,15 @@ import com.inkapplications.ack.structures.AprsPacket
 import com.inkapplications.ack.structures.EncodingConfig
 import com.inkapplications.coroutines.combinePair
 import inkapplications.spondee.measure.Meters
+import inkapplications.spondee.spatial.GeoCoordinates
 import inkapplications.spondee.structure.Kilo
 import inkapplications.spondee.structure.value
 import kimchi.logger.KimchiLogger
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
+import kotlin.math.pow
 
 class InternetDriver internal constructor(
     private val aprsCodec: AprsCodec,
@@ -45,9 +45,21 @@ class InternetDriver internal constructor(
         settings.internetServiceConfiguration
             .combinePair(locationProvider.location)
             .onEach { logger.debug("New Settings/Location pair: $it") }
-            .flatMapLatest { (settings, location) ->
-                callbackFlow {
-                    logger.info("Opening APRS-IS Client to ${settings.host}:${settings.port}")
+            .flatMapLatest { (settings, location) -> connectWithRetry(settings, location) }
+            .onEach { if (it.startsWith('#')) logger.info("APRS-IS: $it") }
+            .filter { !it.startsWith('#') }
+            .mapNotNull { captureStringPacket(it) }
+            .onEach { logger.debug("IS Packet Parsed: $it") }
+            .flowOn(Dispatchers.IO)
+            .collect { incoming.emit(it) }
+    }
+
+    private fun connectWithRetry(settings: ConnectionConfiguration, location: GeoCoordinates): Flow<String> {
+        return callbackFlow {
+            logger.info("Opening APRS-IS Client to ${settings.host}:${settings.port}")
+            var attempts = 0
+            while (true) {
+                try {
                     client.connect(
                         server = settings.host,
                         port = settings.port,
@@ -61,19 +73,23 @@ class InternetDriver internal constructor(
                             }"
                         )
                     ) { read, write ->
+                        logger.info("APRS-IS Connected")
+                        attempts = 0
                         coroutineScope {
                             launch { read.consumeEach { send(it) } }
                             launch { transmitQueue.collect { write.send(it) } }
                         }
                     }
+                } catch (error: CancellationException) {
+                    logger.trace("APRS-IS Connection cancelling", error)
+                    throw error
+                } catch (error: Throwable) {
+                    logger.error("APRS-IS Connection terminated unexpectedly", error)
+                    delay(100*(2.0.pow(attempts)).toLong().coerceAtMost(60_000))
+                    ++attempts
                 }
             }
-            .onEach { if (it.startsWith('#')) logger.info("APRS-IS: $it") }
-            .filter { !it.startsWith('#') }
-            .mapNotNull { captureStringPacket(it) }
-            .onEach { logger.debug("IS Packet Parsed: $it") }
-            .flowOn(Dispatchers.IO)
-            .collect { incoming.emit(it) }
+        }
     }
 
     private suspend fun captureStringPacket(data: String): CapturedPacket? {
