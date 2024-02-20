e8af089e316eb30fd305f932bde3a57880c6b6b5
Renee Vandervelde <Renee@ReneeVandervelde.com>
2024-02-19 21:10:50 -0600

    Update jvm target to java 11

--- aprs-android/build.gradle.kts
+++ aprs-android/build.gradle.kts
@@ -44,11 +44,11 @@ android {
         }
     }
     compileOptions {
-        targetCompatibility = JavaVersion.VERSION_1_8
-        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 }
 
